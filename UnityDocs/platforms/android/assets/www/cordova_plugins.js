247981e0eae25d470e5d1a4cec3edebea821ed18
rrlewis <ronan.lewis@sanderson.com>
2015-08-06 14:14:09 +0100

    test

--- UnityDocs/platforms/android/assets/www/cordova_plugins.js
+++ UnityDocs/platforms/android/assets/www/cordova_plugins.js
@@ -1,7 +1,17 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [];
+module.exports = [
+    {
+        "file": "plugins/io.github.pwlin.cordova.plugins.fileopener2/www/plugins.FileOpener2.js",
+        "id": "io.github.pwlin.cordova.plugins.fileopener2.FileOpener2",
+        "clobbers": [
+            "cordova.plugins.fileOpener2"
+        ]
+    }
+];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "io.github.pwlin.cordova.plugins.fileopener2": "1.0.11"
+}
 // BOTTOM OF METADATA
 });
