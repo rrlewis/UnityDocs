5509a8f1ac929e5aa4bd8b54df66399757aafb8a
rrlewis <ronan.lewis@sanderson.com>
2015-11-18 12:39:52 +0000

    test

---
+++ UnityDocs/www/js/classes.js
@@ -0,0 +1,38 @@
+﻿class ActionSheet {
+    constructor(config, buttonPressed) {
+        var buildConfig = function (config) {
+            var configProperties = ['title', 'buttonLabels', 'androidEnableCancelButton', 'addCancelButtonWithLabel', 'addDestructiveButtonWithLabel'];
+            var optionObj = {}; // the new optionObj to build.
+            var currentProperty;
+            for (var i = 0; i < configProperties.length; i++) { // iterate through the properties the options object can have/be edited.
+                currentProperty = configProperties[i];
+                if (config.hasOwnProperty(currentProperty)) {
+                    // if the config object passed to the constructor has the current property
+                    optionObj[currentProperty] = config[currentProperty];
+                }
+            }
+            optionObj.androidTheme = window.plugins.actionsheet.ANDROID_THEMES.THEME_HOLO_LIGHT; // Standardize theme throughout app.
+        }
+        if (typeof buttonPressed == 'undefined') {
+            throw Error('No callback defined.');
+            return;
+        } else {
+            if (typeof buttonPressed != 'function') {
+                throw Error('Invalid callback defined.');
+                return;
+            }
+        }
+
+        this.options = buildConfig(config);
+        this.onButtonPressed = buttonPressed;
+    }
+
+    show() {
+        console.log("showing sheet");
+        window.plugins.actionsheet.show(this.options, this.onButtonPressed);
+    }
+    close() {
+        window.plugins.actionsheet.show
+    }
+}
+
--- UnityDocs/www/js/helpers.js
+++ UnityDocs/www/js/helpers.js
@@ -388,3 +388,4 @@ function fillForm(formEle, dataObj) {
         }
     }
 }
+
